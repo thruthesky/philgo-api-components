6bca6113693a4ec4b37981cbd5393b9bddd29162
JAEHO SONG <thruthesky@gmail.com>
2018-10-11 21:58:06 +0800

    adding philgo adv

--- adv/adv.component.ts
+++ adv/adv.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { PhilGoApiService } from 'share/philgo-api/philgo-api.service';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import { PhilGoApiService, ApiPost } from 'share/philgo-api/philgo-api.service';
 
 
 @Component({
@@ -7,30 +7,44 @@ import { PhilGoApiService } from 'share/philgo-api/philgo-api.service';
   templateUrl: './adv.component.html',
   styleUrls: ['./adv.component.scss']
 })
-export class AdvComponent implements OnInit {
+export class AdvComponent implements OnInit, AfterViewInit {
 
+  @Input() code: string;
   @Input() place = 'all';
+  post: ApiPost;
   constructor(
     public philgo: PhilGoApiService
   ) {
-    this.loadAdv();
   }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.loadAdv();
+  }
+
   loadAdv() {
 
-
     const today = this.philgo.Ymd();
+    let where = `post_id='ads'`;
+    if ( this.code ) {
+      where += ` AND access_code = '${this.code}' `;
+    } else {
+      where += ` AND varchar_1='${this.place}' AND int_2 >= ${today} AND no_of_first_image > 0 `;
+    }
+
     this.philgo.debug = true;
     this.philgo.postQuery({
-      fields: ` idx, subject, int_1, int_2, varchar_1, varchar_3, varchar_4, varchar_5 `,
-      where: ` post_id='ads' AND varchar_1='${this.place}' AND int_2 >= ${today} AND no_of_first_image > 0 `,
-      orderby: ` `,
+      fields: ` idx, gid, subject, int_1, int_2, varchar_1, varchar_3, varchar_4, varchar_5 `,
+      where: where,
+      orderby: ``,
       limit: 1
     }).subscribe(res => {
       console.log('postQuery: ', res);
+      if (res && res.length) {
+        this.post = res[0];
+      }
     }, e => console.error(e));
 
   }
