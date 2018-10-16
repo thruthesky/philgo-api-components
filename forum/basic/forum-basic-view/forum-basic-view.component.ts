7d731a7289ccad786a95cf0faf4870cfa68cfe2b
JAEHO SONG <thruthesky@gmail.com>
2018-10-16 18:23:19 +0800

    update

--- forum/basic/forum-basic-view/forum-basic-view.component.ts
+++ forum/basic/forum-basic-view/forum-basic-view.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, AfterViewInit } from '@angular/core';
 import { ApiPost, PhilGoApiService } from '../../../../philgo-api/philgo-api.service';
 import { ComponentService } from '../../../service/component.service';
 import { AppService } from '../../../../../projects/pwa/src/services/app.service';
+
+
 
 @Component({
   selector: 'app-forum-basic-view',
   templateUrl: './forum-basic-view.component.html',
   styleUrls: ['../../../scss/index.scss', './forum-basic-view.component.scss']
 })
-export class ForumBasicViewComponent implements OnInit {
+export class ForumBasicViewComponent implements OnInit, AfterViewInit {
 
   @Input() post: ApiPost;
 
@@ -16,14 +18,37 @@ export class ForumBasicViewComponent implements OnInit {
   @Output() edit = new EventEmitter<ApiPost>();
   @Output() delete = new EventEmitter<ApiPost>();
 
+  // @ViewChild('postContent') postContent: HTMLDivElement;
 
+  content;
   constructor(
     public a: AppService,
     public philgo: PhilGoApiService,
     public componentService: ComponentService
-  ) { }
+  ) {
+    window['showImage'] = this.showImage.bind(this);
+  }
+
+  showImage(img) {
+    console.log('popup this image:', img);
+  }
+  ngAfterViewInit() {
+    setTimeout(() => {
+      let content = this.post.content;
+      if (content) {
+        content = content.replace('<img', `<img onclick="showImage(this)"`);
+        this.content = content;
+      }
+    }, 100);
+  }
 
   ngOnInit() {
+
+  }
+
+
+  onClickImage(img) {
+    console.log('img: clicked ', img);
   }
 
   show(post) {
@@ -31,7 +56,7 @@ export class ForumBasicViewComponent implements OnInit {
   }
 
   onReport(post: ApiPost) {
-    if ( post['loaderReport'] ) {
+    if (post['loaderReport']) {
       return;
     }
     post['loaderReport'] = true;
@@ -48,7 +73,7 @@ export class ForumBasicViewComponent implements OnInit {
   }
 
   onVote(post: ApiPost, mode: 'good' | 'bad') {
-    if ( post['loaderVote'] ) {
+    if (post['loaderVote']) {
       return;
     }
     post['loaderVote'] = true;
@@ -63,7 +88,7 @@ export class ForumBasicViewComponent implements OnInit {
     });
   }
   onDelete(post: ApiPost) {
-    if ( post['loaderDelete'] ) {
+    if (post['loaderDelete']) {
       return;
     }
     post['loaderDelete'] = true;
@@ -77,5 +102,6 @@ export class ForumBasicViewComponent implements OnInit {
       post['loaderDelete'] = false;
     });
   }
+
 }
 
